3fe1632170a55e54bbbae363003cc3c6795a5f94
Eseneev Anton <shaman_kub@mail.ru>
2021-11-11 22:19:38 +0300

    Lesson 4. Auth app and user model

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -31,14 +31,18 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "mainapp",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'mainapp',
+    'authnapp',
 ]
+
+# Auth model
+AUTH_USER_MODEL = 'authnapp.ShopUser'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
